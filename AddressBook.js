eae8ce465e91bfa5a544dbbb524e80a5ca5d432a
Arnav@Victor <shubhamsaxena28282@gmail.com>
2025-03-21 11:19:18 +0530

    [Arnav] Added regix

---
+++ AddressBook.js
@@ -0,0 +1,70 @@
+class Contact {
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.validateName(firstName, "First Name");
+        this.validateName(lastName, "Last Name");
+        this.validateAddress(address, "Address");
+        this.validateAddress(city, "City");
+        this.validateAddress(state, "State");
+        this.validateZip(zip);
+        this.validatePhoneNumber(phoneNumber);
+        this.validateEmail(email);
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    validateName(name, fieldName) {
+        const nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
+        if (!nameRegex.test(name)) {
+            throw new Error(`${fieldName} must start with a capital letter and have at least 3 characters.`);
+        }
+    }
+
+    validateAddress(value, fieldName) {
+        if (value.length < 4) {
+            throw new Error(`${fieldName} must have at least 4 characters.`);
+        }
+    }
+
+    validateZip(zip) {
+        const zipRegex = /^[1-9][0-9]{5}$/;
+        if (!zipRegex.test(zip)) {
+            throw new Error(`Zip code must be a 6-digit number and not start with 0.`);
+        }
+    }
+
+    validatePhoneNumber(phone) {
+        const phoneRegex = /^[6-9][0-9]{9}$/;
+        if (!phoneRegex.test(phone)) {
+            throw new Error(`Phone number must be a valid 10-digit number starting with 6-9.`);
+        }
+    }
+
+    validateEmail(email) {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailRegex.test(email)) {
+            throw new Error(`Invalid email format.`);
+        }
+    }
+
+    toString() {
+        return `Name: ${this.firstName} ${this.lastName}\nAddress: ${this.address}, ${this.city}, ${this.state} ${this.zip}\nPhone: ${this.phoneNumber}\nEmail: ${this.email}`;
+    }
+}
+
+// Example Usage
+try {
+    let contact1 = new Contact("Nikhil", "Kumar", "BHEL Jhansi", "Jhansi", "UP", "284120", "9876543210", "nikhil.kumar@gmail.com");
+    console.log(contact1.toString());
+
+    let contact2 = new Contact("Amit", "Sharma", "Delhi NCR", "Delhi", "DL", "110001", "9123456789", "amit.sharma@gmail.com");
+    console.log(contact2.toString());
+} catch (error) {
+    console.error(error.message);
+}
